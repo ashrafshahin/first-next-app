956f54e28d01a65b5d84b405c847e9183a975fb3
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-05-27 10:51:41 +0100

    Build error updated...

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+        <div className="p-10">
+            <h1 className="text-3xl font-bold">
+                Blog Page
+            </h1>
+        </div>
+    );
+}
